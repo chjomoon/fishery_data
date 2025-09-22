9fcc0142acc8f4657484022ee395b5d137b68819
chjomoon <33528293+chjomoon@users.noreply.github.com>
2025-09-22 21:33:42 +0900

    Add files via upload

--- web_pjt/web_app/urls.py
+++ web_pjt/web_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
 
-app_name  = 'firstapp'
+app_name  = 'webapp'
 urlpatterns = [
     path('', views.index, name='home'),
     path('test/', views.index, name='test'),
+    path('pairplot/', views.show_pairplot, name='pairplot'),
+    path('heatmap/', views.show_heatmap, name='heatmap'),
+    path('pjt_desc/', views.project, name='project'),
+    path('predict/', views.predict_view, name='predict'),
 ]
